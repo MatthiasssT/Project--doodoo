4429bfa0cbd7d07199392049ce40b946f8978f77
MatthiasssT <140664830+MatthiasssT@users.noreply.github.com>
2023-08-29 16:11:54 +1000

    basic character scripted
    character basic scene scripted in. also added set up for grass and physical objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //move speed
+    public float moveSpeed;
+    // check for moving
+    private bool isMoving;
+    // get keyboard input
+    private Vector2 input;
+
+    // layers
+    //public LayerMask grassLayer;
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            // handles movement and towards target postion
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            //remove diagonal movement
+            if (input.x != 0) input.y = 0;
+            
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+            
+        }
+    }
+    
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        
+        
+        // check to find target postion and if not there then move
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+
+        //CheckForEncounters();
+    }
+    
+    //private void CheckForEncounters()
+    //{
+        //if (Physics2D.OverlapCircle(transform.position, 0.2, grassLayer) != null)
+        //{
+            //chance of encounter 1 in 10
+            //if (Random.Range(1, 101) <= 10)
+            //{
+                //Debug.Log("encountered a monster");
+            //}
+        //}
+    //}
+
+
+}
